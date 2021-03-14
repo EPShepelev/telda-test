ea3880e59d4db0a263df42d9bec5582bbb6811c4
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-14 21:11:07 +0300

    added todo methods

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  tasks: Task[] = [new Task()];
+
+  constructor() {}
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  updateTask(index: number, updatedTask: Task) {
+    this.tasks[index] = updatedTask;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
