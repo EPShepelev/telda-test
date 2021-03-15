9a13adeb355571ea38e7595adc8a80b986b12883
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-15 07:36:39 +0300

    create addtask func

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -5,7 +5,12 @@ import { Task } from './task.model';
   providedIn: 'root',
 })
 export class DataService {
-  tasks: Task[] = [new Task()];
+  tasks: Task[] = [
+    new Task('This is a text'),
+    new Task(
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam minus natus unde eum sapiente? Architecto recusandae quod laboriosam ab perspiciatis!'
+    ),
+  ];
 
   constructor() {}
 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../shared/task.model';
+import { DataService } from '../shared/data.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.tasks = this.dataService.getAllTasks();
   }
 
+  onFormSubmit(form: NgForm) {
+    this.dataService.addTask(new Task(form.value.text));
+  }
 }
